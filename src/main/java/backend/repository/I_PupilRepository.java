cb5efcbbac94c621f8d05ed47c274e8a99020a9f
DianaGliga11 <diana.adelina.gliga@stud.ubbcluj.ro>
2025-11-05 17:44:27 +0200

    Commented: methods for Class Announcements and Class Schedule Needs to be implemented

--- src/main/java/backend/repository/I_PupilRepository.java
+++ src/main/java/backend/repository/I_PupilRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface I_PupilRepository extends JpaRepository<Pupil,Long> {
     Optional<Pupil> findByEmail(String email);
-    List<Pupil> findByClass_id(Long class_id);
+//    List<Pupil> findByClass_id(Long class_id);
 }
