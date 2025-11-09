b443c8950b8e43a44bf252cd9fc2d8ad35c740ea
Nikolas5516 <nikolas.gavaz@stud.ubbcluj.ro>
2025-11-09 16:32:19 +0200

    User entity done plus changes to app.prop and pom.xml

---
+++ src/main/java/backend/dto/auth/UpdateRequest.java
@@ -0,0 +1,25 @@
+package backend.dto.auth;
+
+import backend.entity.User;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class UpdateRequest {
+    // password optional
+    private String password;
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private User.Type type;
+
+    public UpdateRequest() {}
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public User.Type getType() { return type; }
+    public void setType(User.Type type) { this.type = type; }
+}
