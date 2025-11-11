b4dff1c9cecf79777d0a6f92d0f2f454c29ab373
simona <simonapetian@gmail.com>
2025-11-11 20:59:35 +0200

    pom fixes

--- src/main/resources/db/migration/V1_1_0__parent_table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE parent;
-
-CREATE TABLE parent
-(
-    id BIGINT NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_parent PRIMARY KEY (id),
-    CONSTRAINT uq_parent_email UNIQUE (email)
-);
