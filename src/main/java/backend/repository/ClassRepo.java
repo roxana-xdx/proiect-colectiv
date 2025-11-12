62046da68cc5c304058042d856b63d3a0486e97c
miruna <eastboundsoul@gmail.com>
2025-11-12 18:49:27 +0200

    am adaugat restul functiilor din service + rebase din develop

--- src/main/java/backend/repository/ClassRepo.java
+++ src/main/java/backend/repository/ClassRepo.java
@@ -1,9 +1,12 @@
 package backend.repository;
+import backend.entities.Clasa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
         @Repository
-public interface ClassRepo extends JpaRepository<Class,Long> {
-            Optional<Class> findByTeacherID(int homeroom_teacher_id);
-            Optional<Class> findByName(String name);
+public interface ClassRepo extends JpaRepository<Clasa,Long> {
+            Optional<List<Clasa>> findClassByTeacherID(Long homeroom_teacher_id);
+            Optional<List<Clasa>> findClassByName(String name);
         }
