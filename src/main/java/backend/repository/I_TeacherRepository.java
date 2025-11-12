b09c7117ed9bf7a3de1fb1ff39885547fcc6bbbc
Roxana <hannah.roxana@yahoo.com>
2025-11-12 23:20:44 +0200

    added teacher dto + controller + other changes

--- src/main/java/backend/repository/I_TeacherRepository.java
+++ src/main/java/backend/repository/I_TeacherRepository.java
@@ -1,13 +1,14 @@
 package backend.repository;
 
-import backend.entities.Teacher;
+import backend.entity.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+public interface I_TeacherRepository extends JpaRepository<Teacher, Long> {
 
     Optional<Teacher> findByEmail(String email);
+    boolean existsByUser_Email(String email);
 }
