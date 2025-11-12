fed06b1b9e0f38434ffdcf9c5f15b59705c28354
miruna <eastboundsoul@gmail.com>
2025-11-12 18:49:27 +0200

    am adaugat restul functiilor din service + rebase din develop

--- src/main/java/backend/entities/Clasa.java
+++ src/main/java/backend/entities/Clasa.java
@@ -3,21 +3,21 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 @Entity
 @Table(name="Clasa")
-public Class Clasa {
+public class Clasa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Clasa_id;
+    private Long class_id;
     public Long getId() {
-        return Clasa_id;
+        return class_id;
     }
-    @Column(name="Clasa_name",unique=true,nullable=false)
-    @NotBlank(message="Clasa Name should not be Empty")
-    private String Clasa_name;
-    public String get_Clasa_name() {
-        return Clasa_name;
+    @Column(name="class_name",unique=true,nullable=false)
+    @NotBlank(message="Class Name should not be Empty")
+    private String class_name;
+    public String get_class_name() {
+        return class_name;
     }
-    public void set_Clasa_name(String Clasa_name) {
-        this.Clasa_name = Clasa_name;
+    public void set_class_name(String class_name) {
+        this.class_name = class_name;
     }
     //TO DO: de de-comentat cand avem toate entitatile
     //  @ManyToOne(fetch=FetchType.LAZY)
@@ -35,15 +35,15 @@ public Class Clasa {
     public Clasa() {
     }
 
-    public Clasa(String Clasa_name, Long homeroom_teacher_id) {
-        this.Clasa_name = Clasa_name;
+    public Clasa(String class_name, Long homeroom_teacher_id) {
+        this.class_name = class_name;
         this.homeroom_teacher_id = homeroom_teacher_id;
     }
 
     @Override
     public String toString() {
-        return "Clasa{" +
-                "id=" + Clasa_id +
+        return "Class{" +
+                "id=" + class_id +
                 ", teacher id='" + homeroom_teacher_id + ' ' +
                 '}';
     }
--- src/main/java/backend/service/ClassService.java
+++ src/main/java/backend/service/ClassService.java
@@ -1,9 +1,12 @@
 package backend.service;
-
+import backend.entities.Clasa;
 import backend.repository.ClassRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Validated
@@ -14,17 +17,43 @@ public class ClassService {
         this.classRepository = classRepository;
     }
 
-    public Class createClass(Class clazz) {
+    public Clasa createClass(Clasa clazz) {
         return classRepository.save(clazz);
     }
-    public Class getClassById(Long id) {
+    public Clasa getClassById(Long id) {
         return classRepository.findById(id)
         .orElseThrow(() -> new RuntimeException("No class found with id: " + id));
     }
-//    public Class updateClass(Long id, Class newClass) {
-//        return classRepository.findById(id);
-//
-//    }
+    public Clasa updateClass(Long id, Clasa newClass) {
+        return classRepository.findById(id)
+                .map(current -> {
+                    current.set_homeroom_teacher_id(newClass.get_homeroom_teacher_id()); current.set_class_name(newClass.get_class_name());
+                    return classRepository.save(current);})
+                .orElseThrow(() -> new RuntimeException("No class found with id: " + id));
+    }
 
+    public void deleteClassByID(Long id) {
+        if (!classRepository.existsById(id)) {
+            throw new RuntimeException("No class found with id: " + id);
+        }
+        classRepository.deleteById(id);
+    }
 
+    public List<Clasa> getAllClasses() {
+        return classRepository.findAll();
+    }
+
+    public Optional<List<Clasa>> findClassByTeacherID (Long homeroom_teacher_id) {
+        if(homeroom_teacher_id <= 0) {
+            throw new RuntimeException("Invalid homeroom teacher id");
+        }
+        return classRepository.findClassByTeacherID(homeroom_teacher_id);
+    }
+    public Optional<List<Clasa>> findClassByName (String name) {
+        if(name==null || name.isEmpty()) {
+            throw new RuntimeException("Invalid class name");
+        }
+        return classRepository.findClassByName(name);
+    }
 }
+
